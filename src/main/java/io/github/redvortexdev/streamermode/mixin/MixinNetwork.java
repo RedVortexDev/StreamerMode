2580e935af05512d0297b1a3a843c37753b31023
RedVortex <58099979+RedVortexDev@users.noreply.github.com>
2025-05-05 10:30:19 +0300

    style: NetworkMixin -> MixinNetwork

--- src/main/java/io/github/redvortexdev/streamermode/mixin/MixinNetwork.java
+++ src/main/java/io/github/redvortexdev/streamermode/mixin/MixinNetwork.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientCommonNetworkHandler.class)
-public class NetworkMixin {
+public class MixinNetwork {
 
     @Inject(method = {"onPacketException"}, at = {@At("HEAD")}, cancellable = true)
     private void onPacketException(Packet<?> packet, Exception exception, CallbackInfo ci) {
