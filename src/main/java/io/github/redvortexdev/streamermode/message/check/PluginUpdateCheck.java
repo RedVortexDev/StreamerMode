55c9ec00a83dbf4de7c32e577cc91c87b5e34006
RedVortexDev <58099979+RedVortexDev@users.noreply.github.com>
2025-08-09 13:11:49 +0300

    feat: update fawe update message

--- src/main/java/io/github/redvortexdev/streamermode/message/check/PluginUpdateCheck.java
+++ src/main/java/io/github/redvortexdev/streamermode/message/check/PluginUpdateCheck.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 public class PluginUpdateCheck extends MessageCheck {
 
     private static final Pattern BUYCRAFT_UPDATE_REGEX = Pattern.compile("^A new version of BuycraftX \\([0-9.]+\\) is available\\. Go to your server panel at https://server.tebex.io/plugins to download the update\\.$");
-    private static final Pattern WORLD_EDIT_UPDATE_REGEX = Pattern.compile("^\\(FAWE\\) An update for FastAsyncWorldEdit is available\\. You are \\d+ build\\(s\\) out of date\\.\\nYou are running build \\d+, the latest version is build \\d+\\.\\nUpdate at https://www.spigotmc.org/resources/\\d+/$");
+    private static final Pattern WORLD_EDIT_UPDATE_REGEX = Pattern.compile("^\\(FAWE\\) An update for FastAsyncWorldEdit is available\\. You are \\d+ build\\(s\\) behind.\\nYou are running build \\d+, the latest build is \\d+\\.\\nUpdate at https://ci.athion.net/job/FastAsyncWorldEdit$");
     private static final Pattern VIA_VERSION_UPDATE_REGEX = Pattern.compile("^\\[ViaVersion] There is a newer plugin version available: [0-9.]+(-SNAPSHOT)?, you're on: [0-9.]+(-SNAPSHOT)?");
 
     @Override
